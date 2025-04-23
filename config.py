b9106435fa3632ac20538284d65826f94626040e
blazeblazeshubh <208866318+blazeblazeshubh@users.noreply.github.com>
2025-04-24 01:54:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ PRIVATE_CHAT_ID = -1002634106098
 
 # Authentication cookie
 COOKIE = {
-    "COOKIE": "Paste your cookie here"
+    "COOKIE": "PANWEB=1; csrfToken=zcB-8zYpUSuGJYARruP6cika; lang=en; TSID=UG8Ux11m0tQJhha2h6AYF6HoRuElxvMU; bid_n=18c0b12d393d1a88c44207; _ga=GA1.1.534448201.1700992834; _stripe_mid=63de2b36-0aa9-48d3-b049-d92a35378933041del; ndus-Y-Rose7teHuivLNA0f1oAiHAzBWCLRCIovVHKTb; browserid=cP5XWdCGKn9XXeBSULS9_aCyY3XzUgKTIo5xMGHMbeYH3WJwFiJvauWfTg8=; _ga_06ZNKL8C2E=GS1.1. 1707931403.23.1.1707931712.60.0.0; ndut_fmt=12D0AD9E2CF492FD104EBC622EF5D522561B603C27A46F87CE8864493BDC8353"
 }
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
